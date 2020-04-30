c6e13090ae52bca399b1e64a74733d7ca2cc6b19
Miki <miki.stomatologija@gmail.com>
2020-04-30 20:57:42 +0200

    Creation of Article show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'articles/index'
   # post 'orders/create'
   get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show'
   root to: 'articles#index'
-  # get '/users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
