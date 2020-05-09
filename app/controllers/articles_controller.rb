c1929db1b9d99bc349125bc3d701032c30bfa9ac
Miki <miki.stomatologija@gmail.com>
2020-05-09 20:09:45 +0200

    Update changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,11 +14,24 @@ class ArticlesController < ApplicationController
   def create
     # binding.prygi
     # render plain: params[:article].inspect
-    article = Article.new(article_params)
-    if article.save
-      redirect_to article
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
     else
       render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/articles', to: 'articles#index'
   get '/articles/new', to: 'articles#new', as: :new_article
   get '/articles/:id', to: 'articles#show', as: :article
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch '/articles/:id', to: 'articles#update'
   root to: 'articles#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
