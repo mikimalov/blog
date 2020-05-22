bf852846904ac3dd24e432e572d89f51ec2a37db
Miki <miki.stomatologija@gmail.com>
2020-05-22 14:36:56 +0200

    Changes to Index file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  # get 'articles/index'
-  post '/articles', to: 'articles#create'
-  get '/articles', to: 'articles#index'
-  get '/articles/new', to: 'articles#new', as: :new_article
-  get '/articles/:id', to: 'articles#show', as: :article
-  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch '/articles/:id', to: 'articles#update'
+  # # get 'articles/index'
+  # post '/articles', to: 'articles#create'
+  # get '/articles', to: 'articles#index'
+  # get '/articles/new', to: 'articles#new', as: :new_article
+  # get '/articles/:id', to: 'articles#show', as: :article
+  # get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch '/articles/:id', to: 'articles#update'
+  # delete '/articles/:id', to: 'articles#destroy'
+
   root to: 'articles#index'
+
+  resources :articles
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
