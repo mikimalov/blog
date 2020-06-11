423d8538fd6bea2886ce2d6217d8889cc54edcc7
Miki <miki.stomatologija@gmail.com>
2020-06-11 16:06:41 +0200

    Added Update and Delete actions in Comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,43 @@
+class CommentsController < ApplicationController
+  def new
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build(comment_params)
+    if @comment.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
+    if @comment.update(comment_params)
+     redirect_to @article
+    else
+     render :edit
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to comment.article
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
