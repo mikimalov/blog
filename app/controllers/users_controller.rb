05da9f2df450f6989b4a600847d3242694d34226
Miki <miki.stomatologija@gmail.com>
2020-06-23 20:18:50 +0200

    Sessions implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if logged_in?
+      logged_in_notice
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+     log_in(@user)
      redirect_to @user
     else
       render :new
